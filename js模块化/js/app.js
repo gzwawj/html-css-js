391f9cd7ae3c3c8f3cbf888b54f5d8af06593495
gzwawj <936588357@qq.com>
2018-06-18 18:04:09 +0800

    js模块化

---
+++ js模块化/js/app.js
@@ -0,0 +1,13 @@
+var Model = (function () {
+
+    var index = function (e) {
+        alert(e);
+    };
+    var list = function () {
+        alert("list");
+    };
+    return {index: index, list: list}
+
+})();
+
+
