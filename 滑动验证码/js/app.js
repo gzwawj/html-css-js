f4ce9188ae48b17bf3c616c92bbebdf8aa8350e0
gzwawj <936588357@qq.com>
2018-09-29 08:23:05 +0800

    滑动验证码-修改语法和变量名

--- 滑动验证码/js/app.js
+++ 滑动验证码/js/app.js
@@ -1,93 +1,83 @@
-var canvasfun = (e)=> {
-    var canvas = document.getElementById('c');
-    var can = canvas.getContext('2d');
-    can.clearRect(0,0,300,260);
-    can.fillStyle='rgb(112,116,243)';
-    can.fillRect(0,0,300,260);
-    can.clearRect(100,30,20,20);
+let canvasfun = (e) => {
+    let can1 = document.getElementById('c');
+    let c1 = can1.getContext('2d');
+    c1.clearRect(0, 0, 300, 260);
+    c1.fillStyle = 'rgb(112,116,243)';
+    c1.fillRect(0, 0, 300, 260);
+    c1.clearRect(100, 20, 20, 20);
 
-    var canvas1=document.createElement('canvas');
-    var can1=canvas1.getContext('2d');
-    can1.clearRect(0,0,300,260);
-    can1.fillStyle='rgb(255,0,0)';
-    can1.beginPath();
+    let can2 = document.createElement('canvas');
+    let c2 = can2.getContext('2d');
+    c2.clearRect(0, 0, 300, 260);
+    c2.fillStyle = 'rgb(255,0,0)';
+    c2.beginPath();
 
-    //can.rect(20, 20, 50, 50);
-    can1.moveTo(20+e,20);
-    can1.lineTo(20+e,40);
-    can1.lineTo(40+e,40);
-    can1.lineTo(40+e,20);
-    can1.fill();
+    c2.moveTo(0 + e, 20);
+    c2.lineTo(0 + e, 40);
+    c2.lineTo(20 + e, 40);
+    c2.lineTo(20 + e, 20);
+    c2.fill();
 
-    can.drawImage(canvas1,10,10);
+    c1.drawImage(can2, 0, 0);
 }
 
-var $=function(selector){
+let $ = function (selector) {
         return document.querySelector(selector);
     },
-    box=$('.drag'),
-    bg=$('.bg'),
-    text=$('.text'),
-    btn=$('.btn'),
-    success=false,
-    distance=box.offsetWidth - btn.offsetWidth;
+    box = $('.drag'),
+    bg = $('.bg'),
+    text = $('.text'),
+    btn = $('.btn'),
+    success = false,
+    distance = box.offsetWidth - btn.offsetWidth;
 
+//画布初始化
+canvasfun(0);
 //添加鼠标按下事件
-btn.onmousedown=function(e){
-    console.log(e);
+btn.onmousedown = (e) => {
     btn.style.transition = "";
     bg.style.transition = "";
+    let downX = e.clientX || window.event.clientX;
 
-    var e=e || window.event;
-    var downX=e.clientX;
-
-    document.onmousemove=function(e){
-        var e=e || window.event;
-        var moveX=e.clientX;
-
-        canvasfun(moveX-downX)
-        console.log(10);
-
-        var offsetX=moveX-downX;
-        if(offsetX>distance){
-            offsetX=distance;
+    document.onmousemove = (e) => {
+        let moveX = e.clientX || window.event.clientX;
+        let offsetX = moveX - downX;
+        if (offsetX > distance) {
+            offsetX = distance;
         }
-        if(offsetX<0){
-            offsetX=0;
+        if (offsetX < 0) {
+            offsetX = 0;
         }
-        btn.style.left=offsetX+'px';
-        bg.style.width=offsetX+'px';
-        if(offsetX == distance){
-            text.innerHTML='验证通过';
-            btn.color='lightgreen';
-            btn.innerHTML='&radic;';
-            bg.style.backgroundColor='lightgreen';
+        canvasfun(offsetX)
+        btn.style.left = offsetX + 'px';
+        bg.style.width = offsetX + 'px';
+        if (offsetX == distance) {
+            text.innerHTML = '验证通过';
+            btn.color = 'lightgreen';
+            btn.innerHTML = '&radic;';
+            bg.style.backgroundColor = 'lightgreen';
 
-            success=true;
-
-            btn.onmousedown=null;
-            document.onmousemove=null;
-
+            success = true;
+            btn.onmousedown = null;
+            document.onmousemove = null;
             setTimeout(() => {
                 alert('验证通过');
             }, 1000);
         }
-
-
     }
 
-    document.onmouseup=function(e){
-        if(success){
+    document.onmouseup = (e) => {
+        if (success) {
             return
-        }else{
-            btn.style.left=0;
-            bg.style.width=0;
-            btn.style.transition='left 1s ease';
-            bg.style.transition='width 1s ease';
+        } else {
+            btn.style.left = 0;
+            bg.style.width = 0;
+            btn.style.transition = 'left 1s ease';
+            bg.style.transition = 'width 1s ease';
             canvasfun(0);
         }
-        document.onmousemove=null;
-        document.onmouseup=null;
+        document.onmousemove = null;
+        document.onmouseup = null;
     }
 
 }
